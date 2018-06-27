67d12a76c8caed788c2cf1af8488f202b1702714
Luke Brocke <lukas.brocke@student.kit.edu>
2018-06-27 19:10:39 +0200

    view: Set up main class to initialise the JFrame

--- src/main/java/edu/kit/ipd/pp/viper/view/MainWindow.java
+++ src/main/java/edu/kit/ipd/pp/viper/view/MainWindow.java
@@ -1,39 +1,87 @@
 package edu.kit.ipd.pp.viper.view;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame {
-    public static void main(String[] args) {
-    }
-
     /**
-     *
+     * Window title
      */
-    public void MainWindow() {
-        // TODO
+    private final static String WINDOW_TITLE = "VIPER";
+
+    /**
+     * Window icon
+     * 
+     * The icon path has to begin with a slash,
+     * otherwise Java will look up the file in the package folder, not in
+     * src/main/resources
+     */
+    private final static String WINDOW_ICON = "/viper-icon.png";
+
+    /**
+     * Default window dimensions (however window is resizeable)
+     */
+    private final static int WINDOW_HEIGHT = 600;
+    private final static int WINDOW_WIDTH  = 800;
+
+    /**
+     * Instances of all three panels
+     */
+    private EditorPanel editorPanel;
+    private ConsolePanel consolePanel;
+    private VisualisationPanel visualisationPanel;
+
+    /**
+     * The constructor sets up the {@link JFrame} and initialises all three
+     * panels
+     */
+    public MainWindow() {
+        this.setTitle(WINDOW_TITLE);
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setResizable(true);
+        this.setIconImage(new ImageIcon(getClass().getResource(WINDOW_ICON)).getImage());
+
+        this.editorPanel = new EditorPanel();
+        this.consolePanel = new ConsolePanel();
+        this.visualisationPanel = new VisualisationPanel();
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
 
     /**
-     * @return
+     * Main method, creates a new instance of this class
+     * 
+     * @param args Command line arguments (ignored)
+     */
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+
+    /**
+     * Returns the editor panel
+     * 
+     * @return Editor panel
      */
     public EditorPanel getEditorPanel() {
-        // TODO
-        return null;
+        return this.editorPanel;
     }
 
     /**
-     * @return
+     * Returns the console panel
+     * 
+     * @return Console panel
      */
     public ConsolePanel getConsolePanel() {
-        // TODO
-        return null;
+        return this.consolePanel;
     }
 
     /**
-     * @return
+     * Returns the visualisation panel
+     * 
+     * @return Visualisation panel
      */
     public VisualisationPanel getVisualisationPanel() {
-        // TODO
-        return null;
+        return this.visualisationPanel;
     }
 }
