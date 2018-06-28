445acc48655b5d60916f8392a495c95204c41dbe
Paul Brinkmeier (KIT) <paul.brinkmeier@student.kit.edu>
2018-06-28 16:20:14 +0200

    ast: Add tests for Number

---
+++ src/test/java/edu/kit/ipd/pp/viper/model/ast/NumberTest.java
@@ -0,0 +1,35 @@
+package edu.kit.ipd.pp.viper.model.ast;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class NumberTest {
+    private Number num;
+
+    @Before
+    public void init() {
+        this.num = new Number(42);
+    }
+
+    @Test
+    public void canGetNumberTest() {
+        assertEquals(42, this.num.getNumber());
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("42", this.num.toString());
+    }
+
+    @Test
+    public void toHtmlTest() {
+        assertEquals("42", this.num.toString());
+    }
+
+    @Test
+    public void evaluationTest() throws TermEvaluationException {
+        Number evaluated = this.num.evaluate();
+
+        assertEquals(this.num.getNumber(), evaluated.getNumber());
+    }
+}
