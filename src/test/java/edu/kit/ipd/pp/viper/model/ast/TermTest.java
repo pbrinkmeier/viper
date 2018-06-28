0b6cab2154ffdcc8a6586fd0aaa3413d1e8e9d44
Paul Brinkmeier (KIT) <paul.brinkmeier@student.kit.edu>
2018-06-28 16:36:18 +0200

    ast: Add test for term subclasses

---
+++ src/test/java/edu/kit/ipd/pp/viper/model/ast/TermTest.java
@@ -0,0 +1,14 @@
+package edu.kit.ipd.pp.viper.model.ast;
+
+import java.util.Arrays;
+
+import org.junit.*;
+
+public class TermTest {
+    @Test
+    public void subClasses() {
+        Term fun = new Functor("test", Arrays.asList());
+        Term var = new Variable("X");
+        Term num = new Number(42);
+    }
+}
