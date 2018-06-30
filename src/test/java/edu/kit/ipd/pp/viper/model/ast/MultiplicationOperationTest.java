55c7efa88fe30009458eb7911e04024ca35b9ae5
Paul Brinkmeier (KIT) <paul.brinkmeier@student.kit.edu>
2018-06-30 20:17:46 +0200

    ast: Add tests for BinaryOperations
    - Add tests for AdditionOperation terms
    - Add tests for SubtractionOperation terms
    - Add tests for MutliplicationOperation terms

---
+++ src/test/java/edu/kit/ipd/pp/viper/model/ast/MultiplicationOperationTest.java
@@ -0,0 +1,33 @@
+package edu.kit.ipd.pp.viper.model.ast;
+
+import java.util.Arrays;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+// These tests rely heavily on the Number class; all tests for Number should be fulfilled first
+public class MultiplicationOperationTest {
+    public MultiplicationOperation fortyTwo;
+    public MultiplicationOperation notFortyTwo;
+
+    @Before
+    public void init() {
+        this.fortyTwo = new MultiplicationOperation(new Number(6), new Number(7));
+        this.notFortyTwo = new MultiplicationOperation(new Number(6), new Number(8));
+    }
+
+    @Test
+    public void calculateTest() throws TermEvaluationException {
+        assertEquals(fortyTwo.evaluate(), new Number(42));
+        assertNotEquals(notFortyTwo.evaluate(), new Number(42));
+        assertEquals(notFortyTwo.evaluate(), new Number(48));
+    }
+
+    @Test
+    public void createNewTest() {
+        assertEquals(
+            this.fortyTwo.createNew(Arrays.asList(new Number(1), new Number(2))),
+            new MultiplicationOperation(new Number(1), new Number(2))
+        );
+    }
+}
---
+++ src/test/java/edu/kit/ipd/pp/viper/model/ast/SubtractionOperationTest.java
@@ -0,0 +1,33 @@
+package edu.kit.ipd.pp.viper.model.ast;
+
+import java.util.Arrays;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+// These tests rely heavily on the Number class; all tests for Number should be fulfilled first
+public class SubtractionOperationTest {
+    public SubtractionOperation fortyTwo;
+    public SubtractionOperation notFortyTwo;
+
+    @Before
+    public void init() {
+        this.fortyTwo = new SubtractionOperation(new Number(67), new Number(25));
+        this.notFortyTwo = new SubtractionOperation(new Number(67), new Number(26));
+    }
+
+    @Test
+    public void calculateTest() throws TermEvaluationException {
+        assertEquals(fortyTwo.evaluate(), new Number(42));
+        assertNotEquals(notFortyTwo.evaluate(), new Number(42));
+        assertEquals(notFortyTwo.evaluate(), new Number(41));
+    }
+
+    @Test
+    public void createNewTest() {
+        assertEquals(
+            this.fortyTwo.createNew(Arrays.asList(new Number(1), new Number(2))),
+            new SubtractionOperation(new Number(1), new Number(2))
+        );
+    }
+}
