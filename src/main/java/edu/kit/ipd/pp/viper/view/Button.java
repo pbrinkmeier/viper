2817a7995b7de49c8bd2c0da3c67a9dd92629cff
Luke Brocke <lukas.brocke@student.kit.edu>
2018-07-01 14:23:16 +0200

    view: Remove unused classes

--- src/main/java/edu/kit/ipd/pp/viper/view/Button.java
+++ src/main/java/edu/kit/ipd/pp/viper/view/Button.java
@@ -1,15 +1,44 @@
 package edu.kit.ipd.pp.viper.view;
 
+import java.net.URL;
+
 import javax.swing.JButton;
 
 import edu.kit.ipd.pp.viper.controller.Command;
+import edu.kit.ipd.pp.viper.controller.LanguageManager;
 
 public class Button extends JButton {
     /**
-     * @param textKey 
+     * @param icon
+     * @param tooltipKey
      * @param command
      */
-    public Button(String textKey, Command command) {
-        // TODO
+    public Button(String icon, String tooltipKey, Command command) {
+        super();
+        
+        this.setToolTipText(LanguageManager.getInstance().getString(tooltipKey));
+
+        URL iconURL = this.getClass().getResource(icon);
     }
 }
+
+/*
+ * //Look for the image.
+    String imgLocation = "images/"
+                         + imageName
+                         + ".gif";
+    URL imageURL = ToolBarDemo.class.getResource(imgLocation);
+
+    //Create and initialize the button.
+    JButton button = new JButton();
+    button.setActionCommand(actionCommand);
+    button.setToolTipText(toolTipText);
+    button.addActionListener(this);
+
+    if (imageURL != null) {                      //image found
+        button.setIcon(new ImageIcon(imageURL, altText));
+    } else {                                     //no image found
+        button.setText(altText);
+        System.err.println("Resource not found: " + imgLocation);
+    }
+ */
