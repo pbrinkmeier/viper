24e77ebe1ff44443ea94f23599a3b30439007575
Paul Brinkmeier (KIT) <paul.brinkmeier@student.kit.edu>
2018-07-04 01:15:03 +0200

    interpreter: Implement, document and test the Substitution class

--- src/main/java/edu/kit/ipd/pp/viper/model/interpreter/Substitution.java
+++ src/main/java/edu/kit/ipd/pp/viper/model/interpreter/Substitution.java
@@ -4,27 +4,57 @@ import edu.kit.ipd.pp.viper.model.ast.Term;
 import edu.kit.ipd.pp.viper.model.ast.Variable;
 
 public class Substitution {
+    private final Variable replace;
+    private final Term by;
+
     /**
-     * @param replace
-     * @param by
+     * Initializes a substitution with a variable to replace and a term to replace it with.
+     * This class is immutable.
+     *
+     * @param replace variable to replace
+     * @param by term to replace the variable by
      */
     public Substitution(Variable replace, Term by) {
-        // TODO
+        this.replace = replace;
+        this.by = by;
     }
 
     /**
-     * @return
+     * Getter-method for the variable to replace.
+     *
+     * @return variable to replace
      */
     public Variable getReplace() {
-        // TODO
-        return null;
+        return this.replace;
     }
 
     /**
-     * @return
+     * Getter method for the term to replace the variable by.
+     *
+     * @return term to replace the variable by
      */
     public Term getBy() {
-        // TODO
-        return null;
+        return this.by;
+    }
+
+    /**
+     * Checks whether a substitution is equal to another object.
+     * Two substitutions are equal, if their replace and by fields are equal.
+     *
+     * @param other Object to check equality to
+     * @return whether this is equal to other
+     */
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+
+        if (!other.getClass().equals(Substitution.class)) {
+            return false;
+        }
+
+        Substitution otherSubst = (Substitution) other;
+
+        return otherSubst.getReplace().equals(this.getReplace()) && otherSubst.getBy().equals(this.getBy());
     }
 }
