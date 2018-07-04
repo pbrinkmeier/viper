b7b9e764e0b957138d7a5e2cce7847397354502b
Luke Brocke <lukas.brocke@student.kit.edu>
2018-07-04 16:09:58 +0200

    view: Add JavaDoc to menu class

--- src/main/java/edu/kit/ipd/pp/viper/view/Menu.java
+++ src/main/java/edu/kit/ipd/pp/viper/view/Menu.java
@@ -6,12 +6,20 @@ import java.util.Observer;
 import javax.swing.JMenu;
 import edu.kit.ipd.pp.viper.controller.LanguageManager;
 
+/**
+ * A menu is a wrapper around multiple {@link MenuItem}s or other nested {@link Menu}s.
+ */
 public class Menu extends JMenu implements Observer {
+    /**
+     * The key used for translation
+     */
     private String textKey;
 
     /**
-     * @param textKey
-     * @param command
+     * Creates a new menu using a translation key. {@link MenuItem}s can be added using the inherited method
+     * {@link JMenu#add(javax.swing.JMenuItem)}
+     * 
+     * @param textKey Key used for translation
      */
     public Menu(String textKey) {
         super(LanguageManager.getInstance().getString(textKey));
@@ -21,8 +29,14 @@ public class Menu extends JMenu implements Observer {
         this.textKey = textKey;
     }
 
+    /**
+     * Called by the {@link LanguageManager} when the locale was changed. This triggers an update of the item text.
+     * 
+     * @param obs Class that triggered the update, ignored here
+     * @param obj Object that was triggered, ignored here
+     */
     @Override
-    public void update(Observable o, Object arg) {
+    public void update(Observable obs, Object obj) {
         this.setText(LanguageManager.getInstance().getString(this.textKey));
     }
 }
