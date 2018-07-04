82275d3e6471abae6a866c4f6ba39f8d29ae339e
Luke Brocke <lukas.brocke@student.kit.edu>
2018-07-04 15:48:42 +0200

    view: Add JavaDoc to checkbox class

--- src/main/java/edu/kit/ipd/pp/viper/view/CheckBoxMenuItem.java
+++ src/main/java/edu/kit/ipd/pp/viper/view/CheckBoxMenuItem.java
@@ -5,17 +5,32 @@ import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JCheckBoxMenuItem;
 import edu.kit.ipd.pp.viper.controller.Command;
 import edu.kit.ipd.pp.viper.controller.LanguageManager;
 
+/**
+ * Represents a checkable menu item, used both to enable/disable the standard library via the menubar, as well as for
+ * the language selection.
+ */
 public class CheckBoxMenuItem extends JCheckBoxMenuItem implements ActionListener, Observer {
+    /**
+     * The key used for translation
+     */
     private String textKey;
+
+    /**
+     * Command to execute
+     */
     private Command command;
 
     /**
-     * @param textKey
-     * @param command
+     * Creates a new checkable menu item, that can either be used for a enable/disable item (e.g. enable/disable
+     * standard library) or as a "radio selection" using a {@link ButtonGroup} (e.g. language selection).
+     * 
+     * @param textKey Key used for translation
+     * @param command Command to execute when item was checked/unchecked
      */
     public CheckBoxMenuItem(String textKey, Command command) {
         super(LanguageManager.getInstance().getString(textKey));
@@ -28,14 +43,24 @@ public class CheckBoxMenuItem extends JCheckBoxMenuItem implements ActionListene
         this.addActionListener(this);
     }
 
+    /**
+     * Called when item was checked or unchecked
+     * 
+     * @param event Event that was performed, ignored here
+     */
     @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println("executing command...");
+    public void actionPerformed(ActionEvent event) {
         this.command.execute();
     }
 
+    /**
+     * Called by the {@link LanguageManager} when the locale was changed. This triggers an update of the item text.
+     * 
+     * @param obs Class that triggered the update, ignored here
+     * @param obj Object that was triggered, ignored here
+     */
     @Override
-    public void update(Observable o, Object arg) {
+    public void update(Observable obs, Object obj) {
         this.setText(LanguageManager.getInstance().getString(this.textKey));
     }
 }
