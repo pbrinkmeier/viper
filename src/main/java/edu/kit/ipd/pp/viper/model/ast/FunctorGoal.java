28c41e2211b22d1af13a7345eedb011bbd12633c
Paul Brinkmeier (KIT) <paul.brinkmeier@student.kit.edu>
2018-07-04 18:33:16 +0200

    interpreter: Remove Goal subclasses not necessary for implementation of functor activation records

--- src/main/java/edu/kit/ipd/pp/viper/model/ast/FunctorGoal.java
+++ src/main/java/edu/kit/ipd/pp/viper/model/ast/FunctorGoal.java
@@ -1,6 +1,7 @@
 package edu.kit.ipd.pp.viper.model.ast;
 
 import edu.kit.ipd.pp.viper.model.interpreter.FunctorActivationRecord;
+import edu.kit.ipd.pp.viper.model.interpreter.Interpreter;
 
 import java.util.Optional;
 
@@ -27,13 +28,16 @@ public class FunctorGoal extends Goal {
 
     /**
      * Creates a new FunctorActivationRecord for this goal.
+     * The interpreter reference is passed in order for the activation record
+     * to be able to look up things in the interpreters knowledge base.
      *
+     * @param interpreter interpreter for the created activation record
      * @param parent optional parent ActivationRecord
      * @return new FunctorActivationRecord
      */
     @Override
-    public FunctorActivationRecord createActivationRecord(Optional<FunctorActivationRecord> parent) {
-        return new FunctorActivationRecord(parent, this);
+    public FunctorActivationRecord createActivationRecord(Interpreter interpreter, Optional<FunctorActivationRecord> parent) {
+        return new FunctorActivationRecord(interpreter, parent, this);
     }
 
     /**
--- src/main/java/edu/kit/ipd/pp/viper/model/ast/Goal.java
+++ src/main/java/edu/kit/ipd/pp/viper/model/ast/Goal.java
@@ -2,6 +2,7 @@ package edu.kit.ipd.pp.viper.model.ast;
 
 import edu.kit.ipd.pp.viper.model.interpreter.ActivationRecord;
 import edu.kit.ipd.pp.viper.model.interpreter.FunctorActivationRecord;
+import edu.kit.ipd.pp.viper.model.interpreter.Interpreter;
 
 import java.util.Optional;
 
@@ -9,10 +10,11 @@ public abstract class Goal {
     /**
      * Creates the ActivationRecord for this goal.
      *
+     * @param interpreter interpreter for the created ActivationRecord
      * @param parent optional FunctorActivationRecord that has this as subgoal
      * @return newly created ActivationRecord
      */
-    public abstract ActivationRecord createActivationRecord(Optional<FunctorActivationRecord> parent);
+    public abstract ActivationRecord createActivationRecord(Interpreter interpreter, Optional<FunctorActivationRecord> parent);
 
     /**
      * Getter-method for a string representation of this goal.
