1a31697b45003cd7b44fb8a930552bf3290f71a6
Luke Brocke <lukas.brocke@student.kit.edu>
2018-07-06 20:46:47 +0200

    view: Add LogType enum for printing to the console

---
+++ src/main/java/edu/kit/ipd/pp/viper/view/LogType.java
@@ -0,0 +1,27 @@
+package edu.kit.ipd.pp.viper.view;
+
+/**
+ * A logtype controls the output of the console output field. This mainly includes color, however
+ * certain LogTypes may also affect whether 
+ */
+public enum LogType {
+    /**
+     * Plain text information
+     */
+    INFO,
+
+    /**
+     * Debug messages. These messages only appear if the program was started in debug mode
+     */
+    DEBUG,
+
+    /**
+     * Result of a unification
+     */
+    SUCCESS,
+
+    /**
+     * Error messages of the program
+     */
+    ERROR
+}
