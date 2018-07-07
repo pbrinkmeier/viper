c8b8d430e9966cd4028d7fc7352bf2b2e885f9e6
Luke Brocke <lukas.brocke@student.kit.edu>
2018-07-07 16:13:21 +0200

    view: Add button class for generic buttons without an icon

---
+++ src/main/java/edu/kit/ipd/pp/viper/view/Button.java
@@ -0,0 +1,66 @@
+package edu.kit.ipd.pp.viper.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JButton;
+
+import edu.kit.ipd.pp.viper.controller.Command;
+import edu.kit.ipd.pp.viper.controller.LanguageKey;
+import edu.kit.ipd.pp.viper.controller.LanguageManager;
+
+/**
+ * Represents a {@link Button} with a command to execute on click.
+ */
+public class Button extends JButton implements ActionListener, Observer {
+    /**
+     * Each button is translateable via the {@link LanguageManager}, therefore this translation
+     * key is necessary
+     */
+    private final LanguageKey textKey;
+
+    /**
+     * Command to execute when the button is pressed
+     */
+    private final Command command;
+
+    /**
+     * Creates a new button using a language key for translation of the button text, as well as command to execute on
+     * click.
+     * 
+     * @param textKey Key used for translation of the button text
+     * @param command Command to execute on click
+     */
+    public Button(LanguageKey textKey, Command command) {
+        super(LanguageManager.getInstance().getString(textKey));
+
+        this.textKey = textKey;
+        this.command = command;
+
+        this.addActionListener(this);
+        LanguageManager.getInstance().addObserver(this);
+    }
+
+    /**
+     * Called when button was clicked
+     * 
+     * @param event Event that was performed, ignored here
+     */
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        this.command.execute();
+    }
+
+    /**
+     * Called by the {@link LanguageManager} when the locale was changed. This triggers an update of the button text.
+     * 
+     * @param obs Class that triggered the update, ignored here
+     * @param obj Object that was triggered, ignored here
+     */
+    @Override
+    public void update(Observable obs, Object obj) {
+        this.setText(LanguageManager.getInstance().getString(this.textKey));
+    }
+}
