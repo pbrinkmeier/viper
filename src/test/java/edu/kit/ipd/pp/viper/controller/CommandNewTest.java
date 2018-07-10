67a78cf1df23de0cca3bd4fb380fea12f6a1eb33
Jannik Koch <Jannik.Koch@protonmail.com>
2018-07-10 12:53:44 +0200

    controller: Test format and new, minor style fixes to CommandNew

--- src/test/java/edu/kit/ipd/pp/viper/controller/CommandNewTest.java
+++ src/test/java/edu/kit/ipd/pp/viper/controller/CommandNewTest.java
@@ -1,4 +1,27 @@
 package edu.kit.ipd.pp.viper.controller;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import edu.kit.ipd.pp.viper.view.ConsolePanel;
+import edu.kit.ipd.pp.viper.view.EditorPanel;
+import edu.kit.ipd.pp.viper.view.MainWindow;
+import edu.kit.ipd.pp.viper.view.VisualisationPanel;
+
 public class CommandNewTest {
+    @Test
+    public void getsCleared() {
+        MainWindow gui = new MainWindow(false);
+        gui.setVisible(false);
+        ConsolePanel console = new ConsolePanel(gui);
+        EditorPanel editor = new EditorPanel();
+        VisualisationPanel visualisation = new VisualisationPanel(gui);
+        
+        editor.setSourceText("test");
+        editor.setHasChanged(false);
+        new CommandNew(console, editor, visualisation).execute();
+
+        assertTrue(editor.getSourceText().equals(""));
+    }
 }
