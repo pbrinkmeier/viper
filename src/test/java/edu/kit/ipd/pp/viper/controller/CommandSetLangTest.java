33abe3c511e54c95196ef959482500a0446efb4d
Jannik Koch <Jannik.Koch@protonmail.com>
2018-07-12 16:25:55 +0200

    controller: Add minor tests for toggling the stdlib and language

--- src/test/java/edu/kit/ipd/pp/viper/controller/CommandSetLangTest.java
+++ src/test/java/edu/kit/ipd/pp/viper/controller/CommandSetLangTest.java
@@ -1,5 +1,21 @@
 package edu.kit.ipd.pp.viper.controller;
 
-public class CommandSetLangTest {
+import static org.junit.Assert.assertTrue;
 
+import java.util.Locale;
+
+import org.junit.Test;
+
+public class CommandSetLangTest {
+    /**
+     * Tests whether setting all supported locales works.
+     */
+    @Test
+    public void testAllLocales() {
+        LanguageManager langman = LanguageManager.getInstance();
+        for (final Locale l : langman.getSupportedLocales()) {
+            langman.setLocale(l);
+            assertTrue(l.equals(LanguageManager.getCurrentLocale()));
+        }
+    }
 }
--- src/test/java/edu/kit/ipd/pp/viper/controller/CommandToggleLibTest.java
+++ src/test/java/edu/kit/ipd/pp/viper/controller/CommandToggleLibTest.java
@@ -1,5 +1,41 @@
 package edu.kit.ipd.pp.viper.controller;
 
-public class CommandToggleLibTest {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.kit.ipd.pp.viper.view.ConsolePanel;
+import edu.kit.ipd.pp.viper.view.MainWindow;
+import edu.kit.ipd.pp.viper.view.VisualisationPanel;
+
+public class CommandToggleLibTest {
+    private MainWindow gui;
+    private ConsolePanel console;
+    private VisualisationPanel visualisation;
+    private InterpreterManager interpreterManager;
+
+    /**
+     * Constructs the GUI.
+     */
+    @Before
+    public void buildGUI() {
+        this.gui = new MainWindow(true);
+        this.gui.setVisible(false);
+        this.console = this.gui.getConsolePanel();
+        this.visualisation = this.gui.getVisualisationPanel();
+        this.interpreterManager = this.gui.getInterpreterManager();
+    }
+    
+    /**
+     * Tests twice whether toggling the standard library works properly.
+     */
+    @Test
+    public void switchOnAndOffTest() {
+        boolean firstState = interpreterManager.isStandardEnabled();
+        new CommandToggleLib(console, visualisation, interpreterManager, gui::switchClickableState).execute();
+        assertTrue(interpreterManager.isStandardEnabled() == !firstState);
+        new CommandToggleLib(console, visualisation, interpreterManager, gui::switchClickableState).execute();        
+        assertTrue(interpreterManager.isStandardEnabled() == firstState);
+    }    
 }
