aa40da043ee28af83ac268c2525bb3e38b8fe65e
Luke Brocke <lukas.brocke@student.kit.edu>
2018-07-16 17:20:33 +0200

    controller: Remove tabs from FileFilters class

--- src/main/java/edu/kit/ipd/pp/viper/controller/FileFilters.java
+++ src/main/java/edu/kit/ipd/pp/viper/controller/FileFilters.java
@@ -13,35 +13,35 @@ public class FileFilters {
     /**
      * File filter used for Prolog files.
      */
-	public static final FileFilter PL_FILTER = createFilter(LanguageKey.PROLOG_FILES, "pl");
+    public static final FileFilter PL_FILTER = createFilter(LanguageKey.PROLOG_FILES, "pl");
 
     /**
      * File filter used for TikZ files.
      */
-	public static final FileFilter TIKZ_FILTER = createFilter(LanguageKey.TIKZ_FILES, "tikz");
-	
+    public static final FileFilter TIKZ_FILTER = createFilter(LanguageKey.TIKZ_FILES, "tikz");
+
     /**
      * File filter used for PNG files.
      */
-	public static final FileFilter PNG_FILTER = createFilter(LanguageKey.PNG_FILES, "png");
-	
+    public static final FileFilter PNG_FILTER = createFilter(LanguageKey.PNG_FILES, "png");
+
     /**
      * File filter used for SVG files.
      */
-	public static final FileFilter SVG_FILTER = createFilter(LanguageKey.SVG_FILES, "svg");
+    public static final FileFilter SVG_FILTER = createFilter(LanguageKey.SVG_FILES, "svg");
 
-	private static FileFilter createFilter(LanguageKey key, String extension) {
-		return new FileFilter() {
+    private static FileFilter createFilter(LanguageKey key, String extension) {
+        return new FileFilter() {
 
-			@Override
-			public boolean accept(File f) {
-	            return FilenameUtils.getExtension(f.getName()).toLowerCase().equals(extension) || f.isDirectory();
-			}
+            @Override
+            public boolean accept(File f) {
+                return FilenameUtils.getExtension(f.getName()).toLowerCase().equals(extension) || f.isDirectory();
+            }
 
-			@Override
-			public String getDescription() {
-	            return LanguageManager.getInstance().getString(key);				
-			}
-		};
-	}
+            @Override
+            public String getDescription() {
+                return LanguageManager.getInstance().getString(key);
+            }
+        };
+    }
 }
