78c07ffd3055bae7e0f66cecb0a82a7b2ef6554c
Jannik Koch <Jannik.Koch@protonmail.com>
2018-07-16 16:08:17 +0200

    controller: Make FileFilters implementation less hard-coded

--- src/main/java/edu/kit/ipd/pp/viper/controller/FileFilters.java
+++ src/main/java/edu/kit/ipd/pp/viper/controller/FileFilters.java
@@ -10,60 +10,35 @@ public class FileFilters {
     /**
      * File filter used for Prolog files.
      */
-    public static final FileFilter PL_FILTER = new FileFilter() {
-        @Override
-        public String getDescription() {
-            return LanguageManager.getInstance().getString(LanguageKey.PROLOG_FILES);
-        }
-
-        @Override
-        public boolean accept(File f) {
-            return FilenameUtils.getExtension(f.getName()).toLowerCase().equals("pl") || f.isDirectory();
-        }
-    };
+	public static final FileFilter PL_FILTER = createFilter(LanguageKey.PROLOG_FILES, "pl");
 
     /**
      * File filter used for TikZ files.
      */
-    public static final FileFilter TIKZ_FILTER = new FileFilter() {
-        @Override
-        public String getDescription() {
-            return LanguageManager.getInstance().getString(LanguageKey.TIKZ_FILES);
-        }
-
-        @Override
-        public boolean accept(File f) {
-            return FilenameUtils.getExtension(f.getName()).toLowerCase().equals("tikz") || f.isDirectory();
-        }
-    };
-
+	public static final FileFilter TIKZ_FILTER = createFilter(LanguageKey.TIKZ_FILES, "tikz");
+	
     /**
      * File filter used for PNG files.
      */
-    public static final FileFilter PNG_FILTER = new FileFilter() {
-        @Override
-        public String getDescription() {
-            return LanguageManager.getInstance().getString(LanguageKey.PNG_FILES);
-        }
-
-        @Override
-        public boolean accept(File f) {
-            return FilenameUtils.getExtension(f.getName()).toLowerCase().equals("png") || f.isDirectory();
-        }
-    };
-
+	public static final FileFilter PNG_FILTER = createFilter(LanguageKey.PNG_FILES, "png");
+	
     /**
      * File filter used for SVG files.
      */
-    public static final FileFilter SVG_FILTER = new FileFilter() {
-        @Override
-        public String getDescription() {
-            return LanguageManager.getInstance().getString(LanguageKey.SVG_FILES);
-        }
+	public static final FileFilter SVG_FILTER = createFilter(LanguageKey.SVG_FILES, "svg");
 
-        @Override
-        public boolean accept(File f) {
-            return FilenameUtils.getExtension(f.getName()).toLowerCase().equals("svg") || f.isDirectory();
-        }
-    };
+	private static FileFilter createFilter(LanguageKey key, String extension) {
+		return new FileFilter() {
+
+			@Override
+			public boolean accept(File f) {
+	            return FilenameUtils.getExtension(f.getName()).toLowerCase().equals(extension) || f.isDirectory();
+			}
+
+			@Override
+			public String getDescription() {
+	            return LanguageManager.getInstance().getString(key);				
+			}
+		};
+	}
 }
