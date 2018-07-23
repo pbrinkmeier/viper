aa150e71e6ee42c057f6f88d2eca7bf7d02ab173
SolluxRules <aaronmaier1@gmail.com>
2018-07-23 15:31:21 +0200

    controller: Remove unnecessary visualisation panel in command cancel

--- src/main/java/edu/kit/ipd/pp/viper/controller/CommandCancel.java
+++ src/main/java/edu/kit/ipd/pp/viper/controller/CommandCancel.java
@@ -6,7 +6,6 @@ import edu.kit.ipd.pp.viper.view.VisualisationPanel;
  * Command for canceling the execution of the continue command.
  */
 public class CommandCancel extends Command {
-    private VisualisationPanel visualisation;
     private InterpreterManager interpreterManager;
 
     /**
@@ -16,15 +15,12 @@ public class CommandCancel extends Command {
      * @param interpreterManager Interpreter manager with a reference to the current
      *        interpreter
      */
-    public CommandCancel(VisualisationPanel visualisation, InterpreterManager interpreterManager) {
-        this.visualisation = visualisation;
+    public CommandCancel(InterpreterManager interpreterManager) {
         this.interpreterManager = interpreterManager;
     }
 
     @Override
     public void execute() {
         this.interpreterManager.cancel();
-
-        this.visualisation.setFromGraph(this.interpreterManager.getCurrentVisualisation());
     }
 }
