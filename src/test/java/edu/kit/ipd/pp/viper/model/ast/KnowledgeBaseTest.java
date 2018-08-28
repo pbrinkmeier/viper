86986d5ea8712efb396fc79d21570de69dd8dd03
Paul Brinkmeier (KIT) <paul.brinkmeier@student.kit.edu>
2018-08-28 02:08:33 +0200

    qa: Write tests for KnowledgeBase and rule
    Notes:
    * KnowledgeBase and FunctorUnifier now use Functor#matches instead of checking the name and arity themselves
    * KnowledgeBase#toString has been rewritten to be more concise

---
+++ src/test/java/edu/kit/ipd/pp/viper/model/ast/KnowledgeBaseTest.java
@@ -0,0 +1,57 @@
+package edu.kit.ipd.pp.viper.model.ast;
+
+import java.util.Arrays;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class KnowledgeBaseTest {
+    private KnowledgeBase kb;
+    private KnowledgeBase incomplete;
+
+    @Before
+    public void init() {
+        this.kb = new KnowledgeBase(Arrays.asList(
+            new Rule(Functor.atom("yes"), Arrays.asList()),
+            new Rule(new Functor("gte", Arrays.asList(new Variable("X"), new Variable("Y"))),
+                Arrays.asList(new GreaterThanGoal(new Variable("X"), new Variable("Y")))),
+            new Rule(new Functor("gte", Arrays.asList(new Variable("X"), new Variable("Y"))),
+                Arrays.asList(new EqualGoal(new Variable("X"), new Variable("Y"))))
+        ));
+
+        this.incomplete = new KnowledgeBase(Arrays.asList(
+            new Rule(Functor.atom("yes"), Arrays.asList()),
+            new Rule(new Functor("gte", Arrays.asList(new Variable("X"), new Variable("Y"))),
+                Arrays.asList(new GreaterThanGoal(new Variable("X"), new Variable("Y"))))
+        ));
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("yes.\n\ngte(X, Y) :-\n  X > Y.\ngte(X, Y) :-\n  X =:= Y.\n", this.kb.toString());
+    }
+
+    @Test
+    public void getMatchingRulesTest() {
+        assertEquals(
+            Arrays.asList(new Rule(Functor.atom("yes"), Arrays.asList())),
+            this.kb.getMatchingRules(Functor.atom("yes"))
+        );
+    }
+
+    @Test
+    public void withRuleTest() {
+        assertEquals(
+            this.kb,
+            this.incomplete.withRule(new Rule(new Functor("gte", Arrays.asList(new Variable("X"), new Variable("Y"))),
+                Arrays.asList(new EqualGoal(new Variable("X"), new Variable("Y")))))
+        );
+    }
+
+    // This is basically a dummy for the two edge cases, equals() has been tested above
+    @Test
+    public void equalsTest() {
+        assertNotEquals(this.kb, null);
+        assertNotEquals(this.kb, new Object());
+    }
+}
