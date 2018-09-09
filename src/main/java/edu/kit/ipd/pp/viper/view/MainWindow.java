5982c80fb6f60f0bcb8d2269cf85c5375c7a852c
Paul Brinkmeier (KIT) <paul.brinkmeier@student.kit.edu>
2018-09-09 13:44:16 +0200

    qa: Revert accidental deletion of final modifier for panels

--- src/main/java/edu/kit/ipd/pp/viper/view/MainWindow.java
+++ src/main/java/edu/kit/ipd/pp/viper/view/MainWindow.java
@@ -92,9 +92,9 @@ public class MainWindow extends JFrame {
     /**
      * Instances of all three panels
      */
-    private EditorPanel editorPanel;
-    private ConsolePanel consolePanel;
-    private VisualisationPanel visualisationPanel;
+    private final EditorPanel editorPanel;
+    private final ConsolePanel consolePanel;
+    private final VisualisationPanel visualisationPanel;
 
     private CommandNew commandNew;
     private CommandOpen commandOpen;
